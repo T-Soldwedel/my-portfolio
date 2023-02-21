4915f229f16cf3032bd61cdcd6a1a3d84412e7ea
Sold-T <thomas.soldwedel@gmail.com>
2023-02-21 12:58:56 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Routes, NavLink } from "react-router-dom";
+import Home from "./components/Home";
+import Projects from "./components/Projects";
+import "./styles/App.css";
+import { FaGithub } from "react-icons/fa";
+import { SiGmail } from "react-icons/si";
+import { FaLinkedin } from "react-icons/fa";
 
-function App() {
+const isActive = ({ isActive }) => {
+  return {
+    color: isActive ? "rgba(255, 255, 0, 0.75)" : "",
+    // backgroundColor: isActive ? "rgba(255, 255, 0, 0.75)" : ""
+  };
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="page-container">
+        <header>
+          <div className="name-container">
+            <h1>Thomas Soldwedel</h1>
+            <p>Fullstack Web Developer</p>
+          </div>
+
+          <nav className="navigation">
+            <ul>
+              <li>
+                <NavLink className="navlink" to="/" style={isActive}>
+                  | About me |
+                </NavLink>
+              </li>
+              <li>
+                <NavLink className="navlink" to="/projects" style={isActive}>
+                  | Projects |
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+        </header>
+
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route />
+          </Routes>
+        </main>
+
+        <footer>
+
+          <span>
+            <a href="https://github.com/T-Soldwedel" target="_blank"><FaGithub /></a>
+          </span>
+
+          <span>
+            <a href="https://www.linkedin.com/in/thomas-soldwedel/" target="_blank"><FaLinkedin /></a>
+          </span>
+        </footer>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Projects.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "../styles/Projects.css";
+
+export default function Projects() {
+  return (
+    <div>
+      <div className="all-projects-container">
+        {/* <h5>- Projects (Design & Development) -</h5> */}
+
+        <div className="single-project-container">
+          <a href="https://github.com/T-Soldwedel" target="_blank">
+            {" "}
+            <h3>TrioMio</h3>
+            <p>2022 / Pro bono / Website</p>
+          </a>
+        </div>
+
+        <div className="single-project-container">
+          <a href="https://github.com/T-Soldwedel" target="_blank">
+            {" "}
+            <h3>InPlace Learning Project</h3>
+            <p>2023 / Training / Fullstack App</p>
+          </a>
+        </div>
+
+        <div className="single-project-container">
+          <a href="https://github.com/T-Soldwedel" target="_blank">
+            {" "}
+            <h3>Next.js Learning Project</h3>
+            <p>2023 / Training / Fullstack App</p>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
