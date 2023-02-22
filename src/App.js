a730587b188e177c45dec7a8ef39ce3cf36d904f
Sold-T <thomas.soldwedel@gmail.com>
2023-02-22 13:42:50 +0100

    added color scheme feature

--- src/App.js
+++ src/App.js
@@ -5,35 +5,88 @@ import Projects from "./components/Projects";
 import "./styles/App.css";
 import { FaGithub } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
+import profileImg from "./assets/profileImg.png";
 
 const isActive = ({ isActive }) => {
   return {
     color: isActive ? "rgba(255, 255, 255)" : "",
-    background: isActive ? "rgba(0, 0, 255, 0.12)" : "",
+    background: isActive ? "rgba(0, 0, 255, 0.45)" : "",
   };
 };
 
 const App = () => {
+  window.onload = function () {
+    const greenBtn = document.querySelector("#green");
+    const purpleBtn = document.querySelector("#purple");
+    const blueBtn = document.querySelector("#blue");
+    const blackBtn = document.querySelector("#black");
+
+    purpleBtn.addEventListener("click", () => {
+      document.documentElement.style.setProperty("--first-color", "#3e015195");
+      // set back to ""
+      document.documentElement.style.setProperty("--second-font-color", "");
+      document.documentElement.style.setProperty("--first-font-color", "");
+    });
+    blueBtn.addEventListener("click", () => {
+      document.documentElement.style.setProperty("--first-color", "#0b3f64");
+      document.documentElement.style.setProperty("--second-font-color", "");
+      document.documentElement.style.setProperty("-font-color", "#e8d35e");
+      // set back to ""
+      document.documentElement.style.setProperty("--first-font-color", "");
+    });
+
+    greenBtn.addEventListener("click", () => {
+      document.documentElement.style.setProperty("--first-color", "#026d06a8");
+      // set back to ""
+      document.documentElement.style.setProperty("--second-font-color", "");
+      document.documentElement.style.setProperty("--first-font-color", "");
+    });
+
+    blackBtn.addEventListener("click", () => {
+      document.documentElement.style.setProperty("--first-color", "#000000");
+      document.documentElement.style.setProperty(
+        "--second-font-color",
+        "#e8d35e"
+      );
+      document.documentElement.style.setProperty("--first-font-color", "white");
+    });
+  };
+
   return (
     <>
       <div className="page-container">
         <header>
           <div className="name-container">
-            <h1>Thomas Soldwedel</h1>
-            <p>Fullstack Web Developer</p>
+            <img src={profileImg} alt="profile"></img>
+            <div>
+              <h1>Thomas Soldwedel</h1>
+              <p>Fullstack Web Developer</p>
+            </div>
           </div>
 
           <nav className="navigation">
             <ul>
-              <li>
-                <NavLink className="navlink" to="/" style={isActive}>
-                  About me
-                </NavLink>
-              </li>
-              <li>
-                <NavLink className="navlink" to="/projects" style={isActive}>
-                  Projects
-                </NavLink>
+              <div className="navlinks">
+                <li>
+                  <NavLink className="navlink" to="/" style={isActive}>
+                    About me
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink className="navlink" to="/projects" style={isActive}>
+                    Projects
+                  </NavLink>
+                </li>
+              </div>
+
+              <li className="color-btns">
+                <button id="purple" className="color-btn"></button>
+
+                <button id="green" className="color-btn"></button>
+
+                <button id="blue" className="color-btn"></button>
+
+                <button id="black" className="color-btn"></button>
               </li>
             </ul>
           </nav>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,8 +5,6 @@ export default function Projects() {
   return (
     <div>
       <div className="all-projects-container">
-        
-
         <div className="single-project-container">
           <a
             href="https://t-soldwedel.github.io/bogus-website/"
@@ -27,18 +25,6 @@ export default function Projects() {
           >
             {" "}
             <h3>InPlace Learning Project</h3>
-            <p>2023 / Training / Fullstack App</p>
-          </a>
-        </div>
-
-        <div className="single-project-container">
-          <a
-            href="https://in-place.vercel.app/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            <h3>Next.js Learning Project</h3>
             <p>2023 / Training / Fullstack App</p>
           </a>
         </div>
